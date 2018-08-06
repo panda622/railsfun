56b6082de142df4ea720c6b395f407634c51148d
Bao Dang <baodq@atmarkcafe.org>
2018-08-06 16:04:28 +0700

    add columns price, published, short description to products tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_06_085810) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "price", precision: 6, scale: 2
+    t.boolean "published"
+    t.string "short_description"
+  end
 
 end

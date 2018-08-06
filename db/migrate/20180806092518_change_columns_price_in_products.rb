b698bd7b7fcec09ae433833031c0fab523fce606
Bao Dang <baodq@atmarkcafe.org>
2018-08-06 16:31:13 +0700

    change dicmal of price from 6 to 10 in products table

---
+++ db/migrate/20180806092518_change_columns_price_in_products.rb
@@ -0,0 +1,9 @@
+class ChangeColumnsPriceInProducts < ActiveRecord::Migration[5.2]
+  def up
+    change_column :products, :price, :decimal, precision: 10, scale: 2
+  end
+
+  def down
+    change_column :products, :price, :decimal, precision: 6, scale: 2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_091720) do
+ActiveRecord::Schema.define(version: 2018_08_06_092518) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_08_06_091720) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 10, scale: 2
     t.boolean "published"
     t.integer "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
